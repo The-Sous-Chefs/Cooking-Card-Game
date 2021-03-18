afa5122324237149215edbcd3a1cc8fdaa88e183
Ian Thorne <ian.thorne526@gmail.com>
2021-03-17 21:14:57 -0500

    Added card CSV to the project and got the beginnings of a parser working.

---
+++ Assets/Scripts/Card System/Card.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardType
+{
+    IMMEDIATE,
+    DELAYED,
+    CONTINUOUS
+}
+
+public enum CardClass
+{
+    DAMAGE,         // cards that do damage to enemies
+    HEAL,           // cards that heal the player
+    DEFEND,         // cards that block incoming damage
+    CONTROL,        // cards that prevent enemies from acting
+    SPECIAL         // cards that draw cards, discard cards, generate mana, etc.
+}
+
+/*
+ * NOTE: By being a struct, Card is a "value type," meaning that the variable
+ *       the struct is "stored" in is actually storing the struct's values, so
+ *       copying a struct will copy by-value. Classes in C# are "reference
+ *       types," so copying one will copy by-reference. If we're expecting
+ *       modifications of a copy to be reflected across all copies, this may
+ *       need to be a class.
+ */
+public struct Card
+{
+    //-----------------
+    // member variables
+    //-----------------
+
+    // visual properties of the card
+    public string name              { get; }
+    public int cost                 { get; }
+    public CardType cardType        { get; }
+    public string cardText          { get; }
+
+    // values that drive functionality, provided by a List<int>--in this order
+    public int turnsInPlay          { get; }
+    public int singleDamage         { get; }
+    public int aoeDamage            { get; }
+    public int heal                 { get; }
+    public int draw                 { get; }
+    public int discard              { get; }
+    public int manaRegen            { get; }
+    public float blockPercent       { get; }    // "translated" by constructor
+    public bool stuns               { get; }    // "translated" by constructor
+
+    // values that drive functionality, determined based on the above
+    public List<CardClass> classes  { get; }    // ??????
+    public bool needsTarget         { get; }
+
+    // public Card(string name, string type, int cost, string text, List<int> values)
+    // {
+    //     //
+    // }
+}
---
+++ Assets/Scripts/Card System/CardParser.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardParser : MonoBehaviour
+{
+    void Awake()
+    {
+        TextAsset cardsCSV = Resources.Load<TextAsset>("Cards");
+        List<string> lines = new List<string>(cardsCSV.text.Split('\n'));
+        foreach(string line in lines)
+        {
+            Debug.Log("===========================");
+            foreach(string word in ParseLine(line))
+            {
+                Debug.Log(word);
+            }
+        }
+    }
+
+    private List<string> ParseLine(string line)
+    {
+        List<string> returnMe = new List<string>();
+
+        returnMe.Add("");
+        int currIndex = 0;
+        bool betweenQuotes = false;
+        foreach(char currChar in line)
+        {
+            if(currChar == '"')
+            {
+                betweenQuotes = !betweenQuotes;
+            }
+            else if(currChar == ',')
+            {
+                if(!betweenQuotes)
+                {
+                    returnMe.Add("");
+                    currIndex++;
+                }
+                else
+                {
+                    returnMe[currIndex] += currChar;
+                }
+            }
+            else
+            {
+                returnMe[currIndex] += currChar;
+            }
+        }
+
+        return returnMe;
+    }
+
+    private Card CreateCardFromLine(List<string> lineFields)
+    {
+        //take that list of strings and turn it into a Card struct
+        return new Card();
+    }
+}
