00890ff170f073c0c11615bdcc1fb391d5a02215
Ian Thorne <ian.thorne526@gmail.com>
2021-04-19 18:39:34 -0500

    Added an interface (in a programming sense) for ui stuff.

---
+++ Assets/Scripts/Card System/IUIManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void CardPlayedDelegate(Card card);
+
+/*
+ * This interface can be implemented by both the actual UI for the battle system
+ * and a testing UI. The BattleManager can just have a reference to an IUIManager
+ * and call these methods on it, regardless of which UI it actually is. This will
+ * allow us to separate the backend from our testing UI and also develop the full
+ * game UI.
+ *
+ * NOTE: The parameters passed to these methods may need changing. They're
+ *       somewhat informed by needing to work in both UI contexts (which may be
+ *       bad design). Some methods are also commented out, since implementing
+ *       classes have to implement each method to compile.
+ */
+public interface IUIManager
+{
+    //-------
+    // events
+    //-------
+
+    event CardPlayedDelegate CardPlayedEvent;
+
+    //--------
+    // methods
+    //--------
+
+    void UpdatePlayerHealth(int maxHealth, int currentHealth);
+
+    void UpdatePlayerMana(int maxMana, int currentMana);
+
+    void DrawCard(int cardID);
+
+    void PlayCard();
+
+    void PutCardInDCCS(Card card);
+
+    void RemoveCardFromDCCS(Card card);
+
+    void DeactivateBasicAbilities();
+
+    void ActivateBasicAbilities();
+
+    void UpdateEnemyHealth(int enemyID, int maxHealth, int currentHealth);
+
+    void RemoveEnemy(int enemyID);
+
+    void WinGame();
+
+    void LoseGame();
+}
