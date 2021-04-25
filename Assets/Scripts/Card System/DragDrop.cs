c1aaefbbea4d9fa1c8212294d979e9c7a6e815bb
DD-1111 <56623337+DD-1111@users.noreply.github.com>
2021-04-26 03:54:15 +0800

    Hands UI made, dragging ,clicking, rearranging of hands are working
    Hands UI made, dragging ,clicking, rearranging of hands are working

--- Assets/Scripts/Card System/DragDrop.cs
+++ Assets/Scripts/Card System/DragDrop.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler,IDragHandler, IDropHandler
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler,IDragHandler, IDropHandler,IPointerEnterHandler,IPointerExitHandler
 {
     [SerializeField] private Canvas canvas;
     private RectTransform rectTransform;
@@ -18,8 +18,22 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
         canvasGroup = GetComponent<CanvasGroup>();
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+     //   if (wedonthavecardselected)
+     //   Now it will still trigger while we are dragging another card
+        rectTransform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        rectTransform.localScale = new Vector3(1f, 1f, 1f);
+    }
+
+
     public void OnBeginDrag(PointerEventData eventData)
     {
+        
         canvasGroup.alpha = .6f;
         canvasGroup.blocksRaycasts = false;
         GetComponent<Image>().color = new Color32(146, 217, 255, 255);
@@ -29,16 +43,20 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
     public void OnDrag(PointerEventData eventData)
     {
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
-
+        rectTransform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        transform.SetParent(GameObject.Find("SelectedCard").transform);
 
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        rectTransform.localScale = new Vector3(1f, 1f, 1f);
         canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
         GetComponent<Image>().color = Color.white;
         rectTransform.anchoredPosition = originPos;
+        transform.SetParent(GameObject.Find("CardsSet").transform);
+
     }
 
     public void OnPointerDown(PointerEventData eventData)
