52d3fc888d43e4a5570d2d7251fc4dcf43c8e51c
Yaswin Veluguleti <yaswin.v1@gmail.com>
2021-04-27 15:09:14 -0500

    OOps

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globals {
+    public static Terrain terrain = null;
+    public static GameObject player = null;
+    public static GameObject buildings = null;
+}
